f025e0c0e3e1e2e93ff0b0f144aa4745e25e9721
Bogdan-Mar <57406307+Bogdan-Mar@users.noreply.github.com>
2020-02-15 14:00:03 +0200

    addPreloader

--- js/common.js
+++ js/common.js
@@ -1,28 +1,42 @@
 $(document).ready(function () {
-    var yourNavigation = $(".nav_top");
-    stickyDiv = "sticky";
-    yourHeader = $('.nav_top').height();
+	$(".preloader, .preloader__content").fadeOut();
+	// 	function preloader() {
+	// 	setInterval(() => {
+	// 		let p = $(".preloader");
+	// 		p.css("opacity", 0);
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > yourHeader) {
-            yourNavigation.addClass(stickyDiv);
-        } else {
-            yourNavigation.removeClass(stickyDiv);
-        }
-    });
+	// 		setInterval(
+	// 			() => p.remove(),
+	// 			parseInt(p.css('1s')) * 1000
+	// 		);
+	// 	}, 1000)
+	// }
+	// setInterval(() =>preloader(), 5000);
 
-    jQuery("a.scrollto").click(function () {
-        elementClick = jQuery(this).attr("href")
-        destination = jQuery(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1100);
-        return false;
-    });
+	var yourNavigation = $(".nav_top");
+	stickyDiv = "sticky";
+	yourHeader = $('.nav_top').height();
 
-    $('#burger').click( function () {
-        $(this).closest("#drawer_menu").addClass('menu__mobile_active');
-    });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > yourHeader) {
+			yourNavigation.addClass(stickyDiv);
+		} else {
+			yourNavigation.removeClass(stickyDiv);
+		}
+	});
 
-    $('.closed, .closed_btn').click( function () {
-        $(this).closest("#drawer_menu").removeClass('menu__mobile_active');
-    });
+	jQuery("a.scrollto").click(function () {
+		elementClick = jQuery(this).attr("href")
+		destination = jQuery(elementClick).offset().top;
+		jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1100);
+		return false;
+	});
+
+	$('#burger').click( function () {
+		$(this).closest("#drawer_menu").addClass('menu__mobile_active');
+	});
+
+	$('.closed, .closed_btn').click( function () {
+		$(this).closest("#drawer_menu").removeClass('menu__mobile_active');
+	});
 });
